ee4b3c8ac82cd8f2b798def4d93dd8bce0ff65f1
mouse_484 <escmouse484@gmail.com>
2021-05-17 11:53:02 +0000

    fix: "-" css properties work
    fix #2

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import type { Properties } from 'csstype';
 
 const style = (node: HTMLElement, parameters: Properties<string | number>) => {
   Object.entries(parameters).forEach(([key, value]) => {
+    key = key.replace(/[A-Z]/, (substring) => '-' + substring.toLowerCase());
+
     node.style.setProperty(key, value);
   });
 };
