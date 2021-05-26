b814e84baebb5382b1d066c75c2f750301311cbc
Andreas Rozek <A.Rozek@gmx.de>
2021-05-26 11:22:43 +0200

    feat: Use the style action every time it is updated (#7)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import type { Properties } from 'csstype';
 
 const style = (node: HTMLElement, parameters: Properties<string | number>) => {
-  Object.entries(parameters).forEach(([key, value]) => {
-    key = key.replace(/[A-Z]/, (substring) => '-' + substring.toLowerCase());
+  function update (parameters: Properties<string | number>) {
+    Object.entries(parameters).forEach(([key, value]) => {
+      key = key.replace(/[A-Z]/, (substring) => '-' + substring.toLowerCase());
 
-    node.style.setProperty(key, value);
-  });
+      node.style.setProperty(key, value);
+    });
+  }
+
+  update(parameters); // invoked when component is first mounted
+
+  return { update } // to be invoked whenever component is updated
 };
 
 export default style;
