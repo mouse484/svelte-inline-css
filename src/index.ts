ee04a5bebf8448b1e4302f6d3a653c6cf3e4978c
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-06-04 08:21:19 +0000

    fix: add test

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,25 @@ import type { Properties } from 'csstype';
 type properties = Properties<string | number>;
 type Parameters = properties | properties[];
 
+export const buildStyle = (parameters: Parameters) => {
+  let properties: properties = {};
+  if (Array.isArray(parameters)) {
+    parameters.forEach((value) => {
+      properties = { ...properties, ...value };
+    });
+  } else {
+    properties = parameters;
+  }
+
+  return Object.entries(properties).map(([key, value]) => {
+    key = key.replace(/[A-Z]/, (substring) => '-' + substring.toLowerCase());
+    return [key, value];
+  });
+};
+
 const style = (node: HTMLElement, parameters: Parameters) => {
   const setStyle = (parameters: Parameters) => {
-    let properties: properties = {};
-    if (Array.isArray(parameters)) {
-      parameters.forEach((value) => {
-        properties = { ...properties, ...value };
-      });
-    } else {
-      properties = parameters;
-    }
-
-    Object.entries(properties).forEach(([key, value]) => {
-      key = key.replace(/[A-Z]/, (substring) => '-' + substring.toLowerCase());
-
+    buildStyle(parameters).forEach(([key, value]) => {
       node.style.setProperty(key, value);
     });
   };
